78d857a6e8d620cefe084d5c94c462d203d6c947
stuart tichener <76698984+stuart4404@users.noreply.github.com>
2021-11-06 15:33:29 +0000

    Add files via upload

---
+++ CV_stuart/main.js
@@ -0,0 +1,20 @@
+const navToggle = document.querySelector('.nav-toggle');
+const nav = document.querySelector('.nav');
+let btnOpen = false;
+
+
+navToggle.addEventListener('click', () => {
+    nav.classList.toggle('nav--visible');
+});
+
+
+
+navToggle.addEventListener('click', () =>{
+    if(!btnOpen) {
+        navToggle.classList.add('open');
+        btnOpen = true;
+    } else {
+        navToggle.classList.remove('open')
+        btnOpen = false
+    }
+});
